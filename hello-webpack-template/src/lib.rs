3e6d9a43fc40a2c46037a219e8558d6151cb6886
tamakiii <tamakiii@users.noreply.github.com>
2020-08-09 18:12:22 +0900

    tmp workde

--- hello-webpack-template/src/lib.rs
+++ hello-webpack-template/src/lib.rs
@@ -1,6 +1,9 @@
+extern crate console_error_panic_hook;
+// use std::panic;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
 use web_sys::console;
-
+// use web_sys;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,6 +13,18 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// #[wasm_bindgen]
+// pub fn run() -> Result<(), JsValue> {
+//     panic::set_hook(Box::new(console_error_panic_hook::hook));
+//
+//     let document = web_sys::window().unwrap().document().unwrap();
+//
+//     let p: web_sys::Node = document.create_element("p")?.into();
+//
+//     p.set_text_context(Some("Hello from Rust, WebAssembly and Webpack!"));
+//
+//     Ok(())
+// }
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
